4823bbe54157349ab3f8763ee203cd69a2e83b1c
Laura Sinclair <l.sinclair@tuta.io>
2024-06-26 14:33:23 +0200

    Config for redirect

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import * as path from 'path';
+import pluginPurgeCss from '@mojojoejo/vite-plugin-purgecss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react()],
+	base: process.env.NODE_ENV === 'production' ? '/projects/memoreel/' : '/',
+	server: {
+		open: 'index.html',
+	},
+	plugins: [
+		react(),
+		pluginPurgeCss({
+			variables: true,
+		}),
+	],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
@@ -15,10 +24,12 @@ export default defineConfig({
 			'@images': `${path.resolve(__dirname, 'src/assets/images/')}`,
 			'@services': `${path.resolve(__dirname, 'src/services/')}`,
 			'@pages': path.resolve(__dirname, 'src/pages'),
-			'@utils': path.resolve(__dirname, 'src/utils')
+			'@utils': path.resolve(__dirname, 'src/utils'),
 		},
 	},
-  build: {
-    outDir: 'dist'
-  }
-})
+	build: {
+		outDir: 'dist',
+		cssMinify: true,
+		sourcemap: true,
+	},
+});
